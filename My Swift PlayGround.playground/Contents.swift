0e3cd3a6a69cb5841d9e19652f020f73dfd18c12
Suraj Pawar <surajpawar898@gmail.com>
2018-02-17 13:22:54 +0530

    push

--- My Swift PlayGround.playground/Contents.swift
+++ My Swift PlayGround.playground/Contents.swift
@@ -1,10 +1,94 @@
 //: Playground - noun: a place where people can play
 
+/*
+ * import
+ */
+
 import UIKit
 
-var str = "Hello, playground"
-print("Hi ");
-print(str);
+/*
+ * Variables
+ */
 
-var str1:String
-str1
+// 1- Var (Normal varaible)
+
+    //****** Assigning value directly swift automatically choose the datatype
+    // String
+    var stringValue = "Hello, playground"
+        print("Hi ");
+        print(stringValue);
+
+    // Integer
+    var intValue = 1
+
+
+    // Float
+
+
+    // Double
+
+
+
+
+
+    //***** Assigning data type manually
+    // String
+    var stringManual:String
+    stringManual = "String manual datatype"
+
+
+// 2- Let (Constant)
+let con = "Constant"
+
+
+
+
+/*
+ * Operators
+ */
+
+// 1- Uninary
+
+
+// 2- Binary
+
+
+// 3- Ternary
+
+
+
+
+/*
+ * Strings
+ */
+
+// Automatic string type (Recomanded)
+var str_New = "Hello World"
+
+// Explicit type declaration
+var str_One:String = "Explicit"
+
+// String concatnation
+
+var firstName = "Suraj"
+var secondName = "Pawar"
+
+var str_Concat = firstName + secondName
+var str_ConcatWithSpace = firstName + " " + secondName
+    // String interpolation
+var age = 25
+var str_Interpolation = "\(firstName) \(secondName) age is \(age)"
+
+// String Functions
+
+// 1- Append
+firstName.append("'ji")
+
+// Captilized
+
+var str_BookTitle = "prorgramming with c"
+    // Captilized is property
+str_BookTitle = str_BookTitle.capitalized
+
+
+
